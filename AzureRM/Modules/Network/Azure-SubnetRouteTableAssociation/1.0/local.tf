ddc17d37b16e1b859eb82ea072e36ab2084b49cf
Shekhar Sorot <shekharssorot2002@gmail.com>
2022-02-04 21:09:56 +0530

    Small bugfix in Azure-SubnetRouteTableAssociation

--- AzureRM/Modules/Network/Azure-SubnetRouteTableAssociation/1.0/local.tf
+++ AzureRM/Modules/Network/Azure-SubnetRouteTableAssociation/1.0/local.tf
@@ -2,18 +2,31 @@
 data "azurerm_subscription" "current" {
 }
 
+data "azurerm_subnet" "this" {
+  count = var.subnet.name == null && var.subnet.virtual_network_name == null ? 0 : 1
+  name                  = var.subnet.name
+  resource_group_name   = var.subnet.resource_group_name
+  virtual_network_name  = var.subnet.virtual_network_name
+}
+
+data "azurerm_route_table" "this" {
+  count = var.route_table.name == null && var.route_table.resource_group_name == null ? 0 : 1
+  name                  = var.route_table.name
+  resource_group_name   = var.route_table.resource_group_name
+}
 
 #Create the local variables
 locals {
   subscription_id = data.azurerm_subscription.current.subscription_id
   subnet_id = var.subnet.id == null ? (
-    var.subnet.name == null && var.subnet.virtual_network_name == null && var.subnet.resource_group_name == null ? (
+    var.subnet.name == null && var.subnet.virtual_network_name == null ? (
       var.virtual_networks[var.subnet.virtual_network_tag].subnet[var.subnet.tag].id
-    ) : "/subscriptions/${local.subscription_id}/resourceGroups/${var.subnet.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.subnet.virtual_network_name}/subnets/${var.subnet.subnet.name}"
+    ) : data.azurerm_subnet.this[0].id
   ) : var.subnet.id
+
   route_table_id = var.route_table.id == null ? (
     var.route_table.name == null && var.route_table.resource_group_name == null ? (
       var.route_tables[var.route_table.tag].id
-    ) : "/subscriptions/${local.subscription_id}/resourceGroups/${var.route_table.resource_group_name}/providers/Microsoft.Network/routeTables/${var.route_table.name}"
+    ) : data.azurerm_route_table.this[0].id
   ) : var.route_table.id
 }
